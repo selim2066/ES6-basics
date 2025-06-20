7921daf480ce08eb79ff8a597c182b431b1044a1
selim2066 <121637755+selim2066@users.noreply.github.com>
2025-06-20 11:19:50 +0600

    arraow function started es6

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 
 console.log("bismillahir rahmanir rahim")
+
+// function expression
+const addd = function add(a,b){
+    return a+b;
+}
+
+console.log(addd(10,10))
+
+// ! arrow function
+
+const substract = (a, b)=> a-b;
+
+console.log(substract(10,5))
