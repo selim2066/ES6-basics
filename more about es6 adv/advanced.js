8e5fd8d5022e3b97b4c12d4998837af111e05b4c
selim2066 <121637755+selim2066@users.noreply.github.com>
2025-06-23 23:19:28 +0600

    closer

--- more about es6 adv/advanced.js
+++ more about es6 adv/advanced.js
@@ -1,1 +1,17 @@
-//! thiskeyword, dot notation vs bracket notation
+//! thiskeyword, dot notation vs bracket notation, closer
+
+//closer
+
+function sayNum() {
+    let count = 0;
+    return function () {
+        count++;
+        return count
+    }
+}
+const say1 = sayNum()
+console.log(say1())
+console.log(say1()) 
+console.log(say1())
+console.log(say1())
+console.log(say1())
