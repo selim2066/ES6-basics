9f5aef0c1bec197cf5939bec0d789d875951613a
selim2066 <121637755+selim2066@users.noreply.github.com>
2025-06-26 17:13:20 +0600

    async & await push in ul as a li for display name and id

--- more about js/async.js
+++ more about js/async.js
@@ -13,7 +13,29 @@ myLoader()
     .then(data => console.log('resolved data', data))
     .then(err => console.log('rejected with value', err))
 
-fetch('')
+fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json())
     .then(data => console.log(data))
     .then(err => console.log(err))
+
+
+async function loadData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    for (const user of data) {
+        console.log(user.name)
+        console.log(user.id)
+
+    }
+    displayData(data)
+}
+loadData()
+
+function displayData(users){
+    const ul = document.getElementById("userList")
+    for(const user of users){
+const li = document.createElement('li')
+li.innerText = ` name: ${user.name} and id: ${user.id}`
+ul.appendChild(li)
+    }
+}
